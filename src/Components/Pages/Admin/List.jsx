6faf51f4f22104d04635b383fb7f18f7d866afbe
Gawmane <gawman.e@hotmail.com>
2022-09-22 08:28:55 +0200

    Stars på listen

--- src/Components/Pages/Admin/List.jsx
+++ src/Components/Pages/Admin/List.jsx
@@ -8,13 +8,21 @@ import style from "../../../assets/Style/Login.module.scss"
 import { Layout } from "../../Tools/Layout/Layout"
 import { PostReviews } from "./Post";
 import { Login } from "../Login/Login";
-import { AiOutlineStar } from "react-icons/ai";
+import { AiFillStar } from "react-icons/ai";
 import { BsCardText } from "react-icons/bs";
+
+const colors = {
+    yellow: "#D39D5B",
+    grey: "#a9a9a9"
+
+}
+
 // Function Component til lister
 export const ReviewsList = () => {
     const [data, setData] = useState([]);
     const { event_id } = useParams(0);
     const { loginData } = useAuth(Login)
+    const [ratingValue, setRatingValue] = useState(undefined)
 
 
     // useEffect hook - styring af renders
@@ -43,11 +51,27 @@ export const ReviewsList = () => {
             <>
                 {/* //Mapper data */}
                 {data && data.map((items) => {
+                    // Const til konvatering af dato til dato(01) Måned(maj) år (2000)
                     const date = Moment(items.created).format("DD.MM.YYYY");
+                    //Const med vores data(number) fra apiet så vi kan sætte det ind i vores stars.map
+                    const numStars = (items.num_stars)
+                    //Const med array på 5 så der vises 5 stjerner (5 objekter)
+                    const stars = Array(5).fill(0)
                     return (
                         <article key={items.id} >
-                            <p>{items.num_stars} <AiOutlineStar /> </p>
+                            {stars.map((_, index) => {
+                                return (
+                                    // Laver vores stjerner og måler på om der er data - ratingValue skal være grå da det er de tomme og numStars skal have gul omkring det antal som den har af værdi
+                                    <AiFillStar size={24} key={index}
+                                        color={(ratingValue || numStars) > index ? colors.yellow : colors.grey}
+
+                                    />
+                                )
+                            })}
                             <p>{date}</p>
+                            {/* Til tjek om der er data i numstars
+                            <p>{numStars}</p> 
+                            */}
                             <p>{items.subject}</p>
                             <p>{items.comment}</p>
                             <p>{items.user.firstname} {items.user.lastname}</p>
